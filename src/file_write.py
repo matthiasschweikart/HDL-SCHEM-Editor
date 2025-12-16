13b490ad7cf76b0536d1a368acb6886f3768e026
Matthias Schweikart <matthias.schweikart@gmx.de>
2025-12-16 10:58:53 +0100

    Aborted save-as does not write to actual file, refactoring

--- src/file_write.py
+++ src/file_write.py
@@ -1,57 +1,66 @@
-""" Write the schematic into a JSON file """
+""" 
+Write the schematic into a JSON file:
+command can be: "save", "save_as", "backup"
+The attribute "success" is used by close_this_window(): Without success the window stays opened.
+"""
 from tkinter.filedialog import asksaveasfilename
 from tkinter import messagebox
 import json
 import os
-#import notebook_diagram_tab
 
 class FileWrite():
     def __init__(self, window, design, command):
-        actual_path_name = design.get_path_name()
         self.success = False
-        if command=="backup":
-            if not actual_path_name.startswith("unnamed"):
-                self.__save_in_file(window, design, actual_path_name + ".tmp", actual_path_name + ".tmp")
-        else:
-            if actual_path_name.startswith("unnamed") or command=="save_as":
-                new_path_name = asksaveasfilename(defaultextension=".hse",
-                                            initialfile = design.get_module_name(),
-                                            filetypes=(("HDL-Schem-Editor files","*.hse"),("all files","*.*")))
-                if new_path_name!="":
-                    design.set_path_name(new_path_name)
-            new_or_actual_path_name = design.get_path_name()
-            if new_or_actual_path_name!="" and not new_or_actual_path_name.startswith("unnamed"):
-                self.success = self.__save_in_file(window, design, new_or_actual_path_name, actual_path_name)
-
-    def __save_in_file(self, window, design, new_or_actual_path_name, actual_path_name):
-        # design zoomen auf standard
-        # Falls es mehrere Architectures gibt, müssen alle gezoomt werden.
-        # Oder man bringt sie beim Arch-Wechsel immer auf die Standardgröße.
+        actual_path_name = design.get_path_name()
+        path_name = self._determine_path_name(design, command, actual_path_name)
+        if path_name=="":
+            return
+        design_dictionary = self._get_design_dictionary(window, design, command)
         window.config(cursor="watch")
-        success = False
         try:
-            if not actual_path_name.endswith(".tmp"):
-                zoom_factor = window.write_data_creator_ref.zoom_graphic_to_standard_size(window, design.get_font_size())
-                design_dictionary = design.get_design_dictionary_for_all_architectures()
-                design_dictionary = window.write_data_creator_ref.round_numbers(design_dictionary)
-            else:
-                design_dictionary = design.get_design_dictionary_for_all_architectures()
-            fileobject = open(new_or_actual_path_name, 'w', encoding="utf-8")
-            fileobject.write(json.dumps(design_dictionary, indent=4, default=str))
-            fileobject.close()
-            success = True
-            if not actual_path_name.endswith(".tmp"):
-                window.write_data_creator_ref.zoom_graphic_back_to_actual_size(window, zoom_factor)
-                window.__class__.open_window_dict[window] = new_or_actual_path_name
-                if new_or_actual_path_name!=actual_path_name:
-                    window.quick_access_object.path_name_changed(actual_path_name, new_or_actual_path_name)
-                if os.path.isfile(actual_path_name + ".tmp"):
-                    os.remove(actual_path_name + ".tmp")
-                design.update_window_title(written=True)
+            self.success = self._write_to_file(path_name, design_dictionary)
+            if command!="backup":
+                self._update_data_base(window, design, actual_path_name, path_name)
         except FileNotFoundError:
-            messagebox.showerror("Error in HDL-SCHEM-Editor", "File " + new_or_actual_path_name + " could not be found at write.")
+            messagebox.showerror("Error in HDL-SCHEM-Editor", "File " + path_name + " could not be found at write.")
         except PermissionError:
-            messagebox.showerror("Error in HDL-SCHEM-Editor", "File " + new_or_actual_path_name + " has no write permission.")
-        # design zurück zoomen auf aktuelle Größe
+            messagebox.showerror("Error in HDL-SCHEM-Editor", "File " + path_name + " has no write permission.")
+        except Exception:
+            messagebox.showerror("Error in HDL-SCHEM-Editor", "File " + path_name + " is not writable.")
         window.config(cursor="arrow")
-        return success
+
+    def _determine_path_name(self, design, command, actual_path_name):
+        if command=="backup":
+            if actual_path_name.startswith("unnamed"):
+                return "" # No ".tmp"-file shall be created.
+            return actual_path_name + ".tmp"
+        if command=="save_as" or actual_path_name.startswith("unnamed"):
+            new_path_name = asksaveasfilename(defaultextension=".hse",
+                                              initialfile = design.get_module_name(),
+                                              filetypes=(("HDL-Schem-Editor files","*.hse"),("all files","*.*")))
+            if new_path_name!="":
+                design.set_path_name(new_path_name)
+            return new_path_name # new_path_name is "", if the user aborted the dialog
+        return actual_path_name
+
+    def _get_design_dictionary(self, window, design, command):
+        if command=="backup":
+            return design.get_design_dictionary_for_all_architectures()
+        zoom_factor = window.write_data_creator_ref.zoom_graphic_to_standard_size(window, design.get_font_size())
+        design_dictionary = design.get_design_dictionary_for_all_architectures()
+        design_dictionary = window.write_data_creator_ref.round_numbers(design_dictionary)
+        window.write_data_creator_ref.zoom_graphic_back_to_actual_size(window, zoom_factor)
+        return design_dictionary
+
+    def _write_to_file(self, path_name, design_dictionary):
+        with open(path_name, 'w', encoding="utf-8") as fileobject:
+            fileobject.write(json.dumps(design_dictionary, indent=4, default=str))
+        return True
+
+    def _update_data_base(self, window, design, actual_path_name, path_name):
+        window.__class__.open_window_dict[window] = path_name
+        if path_name!=actual_path_name:
+            window.quick_access_object.path_name_changed(actual_path_name, path_name)
+        if os.path.isfile(actual_path_name + ".tmp"):
+            os.remove(actual_path_name + ".tmp")
+        design.update_window_title(written=True)
