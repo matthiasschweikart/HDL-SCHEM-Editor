26e1fb0fc22da109645b4f874b52922876ee3e8b
Matthias Schweikart <matthias.schweikart@gmx.de>
2026-07-08 18:29:48 +0200

    Implements short-cut for changing lines into comments and vice versa

--- src/widgets/custom_text.py
+++ src/widgets/custom_text.py
@@ -59,7 +59,7 @@ class CustomText(CodeEditor):
         # "block_edit","generated_hdl","interface_packages","interface_generics",
         # "internals_packages","architecture_first_declarations","architecture_last_declarations","log_text"
         text_name,
-        region: dict[Literal["vhdl", "verilog"], str],
+        region: dict[Literal["vhdl", "verilog"], str],  # The region of the HDL which is edited by this widget.
         parser: vhdl_parsing.VhdlParser,
         store_in_design_data=True,  # store_in_design_data=False is only used by block_edit.py and notebook_log_tab.py.
         has_line_numbers=False,
@@ -85,16 +85,18 @@ class CustomText(CodeEditor):
         self.bind("<Control-o>", lambda event: self._open())  # overwrite Control-o of widget (which inserts a new line)
         self.bind("<Control-e>", lambda event: self._edit_in_external_editor(self.window.design))
         self.bind("<Button-1>", lambda event: self.tag_delete("highlight"))  # Needed after using a HDL/message-link.
-        if self.store_in_design_data:  # This text-widget allows edit operationsand stores changes in design data.
+        if self.store_in_design_data:  # This text-widget allows edit operations and stores changes in design data.
             self.bind("<Key>", self._key_event_after_idle)  # Adds overwrite-mode and store-actions.
-            self.bind("<Control-z>", lambda event: self._store_after_undo_redo())  # Adds store-actions to Ctrl-z.
-            self.bind("<Control-y>", lambda event: self._store_after_undo_redo())  # Adds store-actions to Ctrl-y.
-            self.bind("<Control-Z>", lambda event: self._store_after_edit_redo())  # Adds store-actions to Linux-Ctrl-Z.
+            self.bind("<Control-z>", lambda event: self._undo_or_redo_and_store())  # Adds store-actions to Ctrl-z.
+            self.bind("<Control-y>", lambda event: self._undo_or_redo_and_store())  # Adds store-actions to Ctrl-y.
+            self.bind("<Control-Z>", lambda event: self._edit_redo_and_store())  # Adds store-actions to Linux-Ctrl-Z.
             self.bind("<Insert>", lambda event: self._toggle_overwrite())  # Switch between insert/overwrite mode.
+            self.bind("<Control-C>", lambda event: self._toggle_comment())
         elif not self.disabled:  # This text-widget allows edit operations but does not store in design data.
             self.bind("<Key>", self._key_event_after_idle)  # Adds overwrite mode.
             self.bind("<Control-Z>", lambda event: self._edit_redo())  # Adds Linux-Ctrl-Z.
             self.bind("<Insert>", lambda event: self._toggle_overwrite())  # Switch between insert/overwrite mode.
+            self.bind("<Control-C>", lambda event: self._toggle_comment())
         self._define_text_tags(kwargs.get("font"))
 
     def _define_text_tags(self, font):
@@ -159,11 +161,11 @@ class CustomText(CodeEditor):
             self.insert("1.0", new_text)
         self._key_event()  # Emulates key-event and calls store_change_in_text_dictionary_and_add_syntax_highlight_tags.
 
-    def _store_after_undo_redo(self):
+    def _undo_or_redo_and_store(self):
         self.after_idle(self.store_change_in_text_dictionary_and_add_syntax_highlight_tags, True)
         # The Tkinter text widget handles Ctrl-z or Ctrl-y by itself.
 
-    def _store_after_edit_redo(self):
+    def _edit_redo_and_store(self):
         with contextlib.suppress(tk.TclError):  # Exception at redo with empty stack
             self.edit_redo()  # The Tkinter text widget cannot handle Ctrl-Z by itself.
         self.after_idle(self.store_change_in_text_dictionary_and_add_syntax_highlight_tags, True)
@@ -177,6 +179,58 @@ class CustomText(CodeEditor):
     def _toggle_overwrite(self):
         self.overwrite = not self.overwrite
         return "break"
+
+    def _toggle_comment(self):
+        comment_string = "--" if self.window.design.get_language() == "VHDL" else "//"
+        start_line, end_line = self._get_range_of_lines_to_comment_or_uncomment()
+        all_lines_are_comments = self._check_if_all_lines_are_already_comments(start_line, end_line, comment_string)
+        for line_number in range(start_line, end_line + 1):
+            line_content = self.get(f"{line_number}.0", f"{line_number}.end")
+            if all_lines_are_comments:  # All lines are commented, so remove the comment string from each line.
+                self._remove_comment_from_line(line_number, line_content, comment_string)
+            else:  # At least one line is not commented, so add the comment string to all lines.
+                self._change_line_into_a_comment(line_number, line_content, comment_string, start_line)
+        self.format_after_idle(event=None)
+        return "break"
+
+    def _get_range_of_lines_to_comment_or_uncomment(self) -> tuple[int, int]:
+        if self.tag_ranges(tk.SEL):
+            start_index = self.index(tk.SEL_FIRST)
+            start_line = int(start_index.split(".", maxsplit=1)[0])
+            end_index = self.index(tk.SEL_LAST)
+            if end_index.endswith(".0"):
+                end_line = int(end_index.split(".", maxsplit=1)[0]) - 1
+            else:
+                end_line = int(end_index.split(".", maxsplit=1)[0])
+        else:
+            start_line = int(self.index(tk.INSERT).split(".", maxsplit=1)[0])
+            end_line = start_line
+        return start_line, end_line
+
+    def _check_if_all_lines_are_already_comments(self, start_line, end_line, comment_string):
+        for line_number in range(start_line, end_line + 1):
+            line_content = self.get(f"{line_number}.0", f"{line_number}.end")
+            if not line_content.lstrip().startswith(comment_string):
+                return False
+        return True
+
+    def _change_line_into_a_comment(self, line_number, line_content, comment_string, start_line):
+        number_of_leading_blanks = len(re.search(r"^\s*", line_content).group(0))
+        self.insert(f"{line_number}.{number_of_leading_blanks}", comment_string + " ")
+        if line_number == start_line and self.tag_nextrange(tk.SEL, f"{line_number}.0"):
+            self._extend_selection_to_new_line_start(line_number)
+
+    def _extend_selection_to_new_line_start(self, line_number):
+        self.tag_add(tk.SEL, f"{line_number}.0", tk.INSERT)
+
+    def _remove_comment_from_line(self, line_number, line_content, comment_string):
+        match_object = re.search(r"(^\s*)" + comment_string + r"(.?)", line_content)
+        comment_string_start_index = match_object.start() + len(match_object.group(1))
+        comment_string_end_index = comment_string_start_index + len(comment_string)
+        ends_with_blank = match_object.group(2) == " "
+        if ends_with_blank:
+            comment_string_end_index += 1
+        self.delete(f"{line_number}.{comment_string_start_index}", f"{line_number}.{comment_string_end_index}")
 
     def store_change_in_text_dictionary_and_add_syntax_highlight_tags(self, signal_design_change):
         """Store change in design_data dictionary."""
